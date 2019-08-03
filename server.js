b652718226e9223b67438d451b74184a51d57683
Dallas <dallas.oliver91@gmail.com>
2019-08-02 20:28:37 -0400

    socket connection established, emitting messages

--- server.js
+++ server.js
@@ -1,49 +1,24 @@
 var app = require("express")();
-var server = require("http").Server(app);
+var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
-server.listen(3000);
-// WARNING: app.listen(80) will NOT work here!
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
-
-  if (req.url.indexOf(".html") != -1) {
-    //req.url has the pathname, check if it conatins '.html'
-
-    fs.readFile(__dirname + "/public/index.html", function(err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  }
-
-  if (req.url.indexOf(".js") != -1) {
-    //req.url has the pathname, check if it conatins '.js'
-
-    fs.readFile(__dirname + "/public/index.js", function(err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, { "Content-Type": "text/javascript" });
-      res.write(data);
-      res.end();
-    });
-  }
-  if (req.url.indexOf(".css") != -1) {
-    //req.url has the pathname, check if it conatins '.css'
-
-    fs.readFile(__dirname + "/public/styles.css", function(err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, { "Content-Type": "text/css" });
-      res.write(data);
-      res.end();
-    });
-  }
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", function(socket) {
-  socket.emit("news", { hello: "world" });
-  socket.on("my other event", function(data) {
-    console.log(data);
+  console.log("Client connected.");
+  socket.on("chat message", msg => {
+    console.log(`message: ${msg}`);
+    io.emit("chat message", msg);
   });
+
+  // Disconnect listener
+  socket.on("disconnect", function() {
+    console.log("Client disconnected.");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on port 3000");
 });
