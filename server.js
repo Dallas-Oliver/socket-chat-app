85de6e30fbd847c8a9c669b9c18dee066a75f600
Dallas <dallas.oliver91@gmail.com>
2019-08-04 23:02:03 -0400

    username is now randomly assigned. working on some styling

--- server.js
+++ server.js
@@ -8,24 +8,19 @@ app.get("/", (req, res) => {
 
 io.on("connection", function(socket) {
   console.log("Client connected.");
-  socket.on("chat message", msg => {
-    console.log(`message: ${msg}`);
-    socket.broadcast.emit("chat message", msg);
-  });
-
-  // Disconnect listener
-  socket.on("disconnect", function() {
-    console.log("Client disconnected.");
-  });
 
   socket.on("welcome msg", msg => {
     console.log(msg);
     socket.broadcast.emit("welcome msg", msg);
   });
 
-  socket.on("username", username => {
-    console.log(`${username} joined the chat`);
-    socket.broadcast.emit("username", username);
+  socket.on("chat message", msg => {
+    socket.broadcast.emit("chat message", msg);
+  });
+
+  // Disconnect listener
+  socket.on("disconnect", function() {
+    console.log("Client disconnected.");
   });
 });
 
